10a0ece5f23379498e57861331f4bc3f32a1af53
kwonseozero <kwonseozero@naver.com>
2024-04-26 16:38:14 +0900

    풋터 생성

---
+++ pheonix-react/src/design/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <p>(07212)서울특별시 영등포구 선유동2로 57 이레빌딩</p>
+            <p>대표이사 : 왕혜진 | 사업자 등록번호 : 2023-12-0420 | 통신판매업신고번호 : 2024-서울영등포-0421</p>
+            <p>호스팅사업자 : KH정보교육원 | 개인정보보호 책임자 : 황인빈</p>
+            <p>©KH PHOENIX. ALL Rights Reserved</p>
+        </footer>
+    );
+}
+
+
+export default Footer;
