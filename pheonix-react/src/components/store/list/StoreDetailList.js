7eb38557938d5b4b77662f953c3241196f5b8c14
ggingggang05 <yllee221b@gmail.com>
2024-04-30 21:15:07 +0900

    상세 수정 주

--- pheonix-react/src/components/store/list/StoreDetailList.js
+++ pheonix-react/src/components/store/list/StoreDetailList.js
@@ -3,6 +3,8 @@ import axios from "../../utils/CustomAxios";
 import StoreMenu from "../StoreMenu";
 import Notification from "./Notification";
 import { useParams } from "react-router";
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from "react-icons/fa";
 
 const StoreDetailList = ()=>{
 
@@ -27,21 +29,42 @@ const StoreDetailList = ()=>{
     return(
         <>
             <StoreMenu />
-            
+
+            {/* 제품명 */}
+            <div className="row justify-content-center">
+                <div className="col-lg-8 content-body">
+                    <span style={{ fontSize: '25px' }} className='ms-2'>{products.productName}</span>
+                    <hr></hr>
+                </div>
+            </div>
             <div className="row justify-content-center">
                 <div className="col-lg-8 content-body">
                     <div className="row">
-                            <div className="col-md-4 mb-4" >
+                            <div className="col-md-4 mb-4">
                                 <div className='mt-2'>
-                                    <div>
                                     이미지
-                                    </div>
-                                    <span style={{ fontSize: '25px' }} className='ms-2'>{products.productName}</span>
                                 </div>
-                                <br />
-
-                                    <span>{products.productContent}</span><br />
+                                <div>
+                                    {/* ul로 하기 */}
                                     <span>{products.productPrice}원</span>
+                                    <hr></hr>
+                                    <span>상품 구성 {products.productContent}</span><br />
+                                    <span>유효 기간</span><br />
+                                    <span>원산지 {products.productOrigin}</span><br />
+                                    <span>상품 교환 {products.productContent}</span><br />
+                                    <hr></hr>
+                                    {/* 개수 자리 */}
+                                
+                                
+                                    <Link to={`/cart/${products.productNo}`} className='btn  btn-outline-primary'><FaShoppingCart /></Link>
+                                    <Link to={`/gift/${products.productNo}`} className='btn  btn-outline-primary'>선물하기</Link>
+                                    <Link to={`/purchase/${products.productNo}`} className='btn  btn-outline-primary'>구매하기</Link>
+                                
+                                
+                                
+                                
+                                
+                                </div>  
                             </div>
                     </div>
                 </div>
