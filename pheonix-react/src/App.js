984788ac81b7c5c031b8d7c3397c6507d7d34250
hyedni <hyedni@naver.com>
2024-05-12 23:37:30 +0900

    Nonuser

--- pheonix-react/src/App.js
+++ pheonix-react/src/App.js
@@ -34,7 +34,7 @@ import { useCallback, useEffect } from "react";
 import PersonalDetail from './components/service/PersonalDetail';
 import Pagination from './components/service/Pagination';
 import Bunsil from './components/service/Bunsil';
-import NonUser from './components/user/nonUser'; 
+import NonUser from './components/user/NonUser'; 
 import AddTheater from './components/admin/AddTheater';
 import BookingListPage from './components/booking/BookingListPage';
 import BookingAdd from './components/booking/BookingAdd';
