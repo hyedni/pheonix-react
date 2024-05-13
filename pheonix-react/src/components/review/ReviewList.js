0bea2efe4909179b815de56b55a0ce94f0c81684
ggingggang05 <yllee221b@gmail.com>
2024-05-13 17:22:23 +0900

    좋아요 구현 중

--- pheonix-react/src/components/review/ReviewList.js
+++ pheonix-react/src/components/review/ReviewList.js
@@ -8,16 +8,22 @@ import { useParams } from "react-router";
 import birdImg from "./image/bird.png";
 import eggImg from "./image/egg.png";
 
+//아이콘 임포트
+import { AiFillLike } from "react-icons/ai";
+
 const ReviewList = () => {
 
     const [userId] = useRecoilState(loginIdState);
     const { movieNo } = useParams();
-    const [reviewLists, setReviewLists] = useState([]);
+    const [ reviewLists, setReviewLists ] = useState([]);
+    const [ likeVO, setLikeVO ] = useState([]);
+    const [ finalLists, setFinalListS] = useState([]);
 
     useEffect(() => {
         loadReviews();
-    }, [reviewLists]);
+    }, []);
 
+    //리뷰 글 불러오기
     const loadReviews = useCallback(() => {
         try {
             axios.get(`/review/${movieNo}`).then(resp => {
@@ -29,12 +35,30 @@ const ReviewList = () => {
         }
     }, [movieNo]);
 
+    //좋아요 개수 불러오기
+    const loadLikes = useCallback(()=>{
+        try {
+            const dataList = reviewLists.map(review => ({
+                userId : userId,
+                reviewNo : review.reviewNo
+            }));
+            axios.get("/review_like/checkAll"+ dataList).then(resp => {
+                setLikeVO(resp.data); //리뷰 번호 + 사용자 좋아요 여부 + 글에 대한 좋아요 개수
+                console.log(resp.data);
+            })
+
+        }
+        catch (error) {
+            console.error("API 호출 중 오류 발생:", error);
+        }
+    }, [reviewLists]);
+
     return (
         <>
 
             <div className='row justify-content-center'>
                 <div className='col-lg-8'>
-                    <div className="row "  style={{ borderBottom: 'solid black 1px' }}>
+                    <div className="row " style={{ borderBottom: 'solid black 1px' }}>
                         <div className="col-6">
                             최신순
                         </div>
@@ -49,31 +73,42 @@ const ReviewList = () => {
             <div className='row justify-content-center'>
                 <div className='col-lg-8 content-body'>
                     <div className="row">
-                        {reviewLists.map(review => (
-                            <div className="col-6" key={review.reviewNo}>
-                                <div className="row">
-                                    <div className="col-3">
-                                        {review.userImgLink}
+                        {reviewLists.length === 0 ? ( //리뷰가 있/없 판단
+                            <>
+                                <h1>리뷰가 아직 없어요</h1>
+                            </>
+                        ) : (
+                            <>
+                                {reviewLists.map(review => (
+                                    <div className="col-6" key={review.reviewNo}>
+                                        <div className="row">
+                                            <div className="col-3">
+                                                {review.userImgLink}
+                                            </div>
+                                            <div className="col-9">
+                                                <span>
+                                                    {review.reviewReputation === 'Y' ? (
+                                                        <img src={birdImg} alt="좋은평가" style={{ width: '30px' }} />
+                                                    ) : (
+                                                        <img src={eggImg} alt="나쁜평가" style={{ width: '30px' }} />
+                                                    )}
+                                                    {review.userNick === null ? review.userId : review.userNick}
+                                                </span>
+                                                <p>{review.reviewContent}</p>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col center">
+                                                {new Date(review.reviewDate).toLocaleDateString()}
+                                                 | <AiFillLike/>
+                                                 
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="col-9">
-                                        <span>
-                                            {review.reviewReputation === 'Y' ? (
-                                               <img src={birdImg} alt="좋은평가" style={{ width: '30px'}} />
-                                            ) : (
-                                                <img src={eggImg} alt="나쁜평가" style={{ width: '30px' }} />
-                                            )}
-                                            {review.userNick === null ? review.userId : review.userNick} 
-                                        </span>
-                                        <p>{review.reviewContent}</p>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col center">
-                                        {new Date(review.reviewDate).toLocaleDateString()} | 좋아요
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
+                                ))}
+                            </>
+
+                        )} 
                     </div>
                 </div>
             </div>
