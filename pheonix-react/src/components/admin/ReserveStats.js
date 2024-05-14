574f96ce8ac8e97b2013f915b020759d1da1882f
hyedni <hyedni@naver.com>
2024-05-14 18:47:46 +0900

    예매율

--- pheonix-react/src/components/admin/ReserveStats.js
+++ pheonix-react/src/components/admin/ReserveStats.js
@@ -1,9 +1,33 @@
 import './RserveStats.css';
+import axios from "../utils/CustomAxios";
+import { useCallback, useEffect, useState } from 'react';
 
 function ReserveStats() {
+
+    const [dataAll, setDataAll] = useState([]);
+
+    const today = new Date();
+    function toChar() {
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0'); 
+        const day = String(today.getDate()).padStart(2, '0'); 
+        const formattedDate = `${year}-${month}-${day}`;
+        return formattedDate;
+    }
+
+    const loadListAll = useCallback(async ()=>{
+        const date = toChar();
+        const resp = await axios.get(`/stats/${date}`);
+        setDataAll(resp.data);
+        console.log(resp.data);
+    },[]);
+
+    useEffect(()=>{
+        loadListAll();
+    },[]);
+
     return (
         <>
-
             <br />
             <br />
             {/* 페이지 제목 */}
@@ -26,8 +50,39 @@ function ReserveStats() {
                 </div>
             </div>
 
+            <div className="row">
+                <div className="offset-2 col-lg-8">
+                <div className='row mt-3 mb-3'>
+                    <div className='col'>
+                        <div className='text-end'>집계일시: {toChar()}</div>
+                    </div>
+                </div>
+                <table className='table table-hover table-rate mb-5'>
+                    <thead>
+                        <tr style={{fontSize:'15px', fontWeight:'bolder'}}>
+                            <th>순위</th>
+                            <th>영화제목</th>
+                            <th>개봉일</th>
+                            <th>예매관객수</th>
+                            <th>예매율</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {dataAll.map((data, index)=>(
+                            <tr key={data.reserveStatsNo}>
+                                <td>{index + 1}</td>
+                                <td>{data.movieTitle}</td>
+                                <td>{data.movieOpenDate}</td>
+                                <td>{data.reserveStatsMovie}명</td>
+                                <td>{data.reserveStatsRate}%</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                
+                </div>
+            </div>
 
-            
 
 
         </>
