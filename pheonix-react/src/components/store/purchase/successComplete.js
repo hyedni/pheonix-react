d85a1dad4ce7abd617abc56f8e5c858083e1858d
ggingggang05 <yllee221b@gmail.com>
2024-05-16 16:26:57 +0900

    구매내역 페이지 이동 링크 추가

--- pheonix-react/src/components/store/purchase/successComplete.js
+++ pheonix-react/src/components/store/purchase/successComplete.js
@@ -19,7 +19,7 @@ const successComplete = () => {
                     <div className='row' style={{marginBottom: '100px'}}>
                         <h4>결제해주셔서 감삼다!!!</h4>
                         <div className='col-6 mt-4'>
-                            <button className='btn btn-dark w-100 btn-lg'>구매내역 보기</button>
+                            <Link to={`/mypage/mystore`} className='btn btn-dark w-100 btn-lg'>구매내역 보기</Link>
                         </div>
                         <div className='col-6 mt-4'>
                             <Link to={`/store/package`} className='btn btn-dark w-100 btn-lg'>스토어 가기</Link>
