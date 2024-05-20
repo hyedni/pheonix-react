b5096d6ce16648fc25c1f1f851fc0dbc55f738dd
hyedni <hyedni@naver.com>
2024-05-20 10:50:57 +0900

    경로수정

--- pheonix-react/src/components/user/Login.js
+++ pheonix-react/src/components/user/Login.js
@@ -58,7 +58,7 @@ function Login() {
             if (user.userId.length === 0) throw new Error("사용자 ID를 입력하세요.");
             if (user.userPw.length === 0) throw new Error("비밀번호를 입력하세요.");
 
-            const resp = await axios.post(`http://localhost:8080/user/login`, user);
+            const resp = await axios.post(`http://192.168.30.37:8080/user/login`, user);
             console.log(resp.data);
             setLoginId(resp.data.userId);
             setLoginGrade(resp.data.userGrade);
